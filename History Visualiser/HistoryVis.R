18bb7915c467b5820ae255b3671edbd9bcd5f1ef
Adam Cooper <a.r.cooper@bolton.ac.uk>
2012-02-17 21:44:11 +0000

    doh

--- History Visualiser/HistoryVis.R
+++ History Visualiser/HistoryVis.R
@@ -69,7 +69,7 @@ corp<-Corpus(DataframeSource(table), readerControl=list(reader= readTabular(mapp
 stop.words<-CustomStopwords()
 corp<-tm_map(corp,removeNumbers)
 corp<-tm_map(corp2,removePunctuation)
-dtm.tf<-DocumentTermMatrix(corp2, control=list(stemming=TRUE, stopwords=stop.words, minWordLength=3))
+dtm.tf<-DocumentTermMatrix(corp, control=list(stemming=TRUE, stopwords=stop.words, minWordLength=3))
 dtm.bin<-weightBin(dtm.tf)
 # pull out the sentiment data
 pos.score<-unlist(meta(corp,tag="Positive", type="local"))
