7d2ed4d443ca3c18a3c7700a2e154d6fe23845e8
Adam Cooper <a.r.cooper@bolton.ac.uk>
2012-09-14 18:32:30 +0100

    latest run params

--- Adjacent Words/TEL Blogs 20090101-20120912 Init.R
+++ Adjacent Words/TEL Blogs 20090101-20120912 Init.R
@@ -2,7 +2,7 @@
 ## Run Properties - dependent on the source
 base.dir<-"/home/arc1/R Projects/Text Mining Weak Signals"
 source.dir<-paste(base.dir,"Source Data",sep="/")
-set.name<-"Union C 2006 to 2011"
+set.name<-"TEL Blogs 20090101-20120912" #<<<<<<<<<<<<< edit me - for the output dir
 output.dir<-paste("/home/arc1/R Projects/Text Mining Weak Signals Output/Adjacent Words",set.name,sep="/")
 #brew.dir<-paste(base.dir,"AdjacentWords",sep="/")
 
@@ -12,30 +12,24 @@ setwd(output.dir)
 ## SOURCE DATA SELECTION
 # Either a) [DEPRECATED] list csv files "with metrics" as produced by Pre-process.R These are combined into one corpus or
 #        b) Locate a SQLite Database and define a query to extract
-#sets.csv <- c("ICALT Abstracts 2005-2011 with metrics.csv",
-#"ECTEL Abstracts 2006-2011 with metrics.csv",
-#"ICWL Abstracts 2005-2011 with metrics.csv",
-#"ICHL Abstracts 2008-2011 with metrics.csv",
-#                   "CAL Abstracts 2007-2011 with metrics.csv")
 set.csv<-NA
 sqlite.filename<-"TMWS Data A.sqlite" #set this to NA to use [deprecated] option a
-sql<-"SELECT year, title, abstract FROM abstract WHERE year BETWEEN '2005' AND '2011'"
+sql<-"SELECT content,datestamp FROM blog_post WHERE datestamp BETWEEN '2009-01-01' AND '2012-09-12'"
 use.sqlite<-!is.na(sqlite.filename)
 
-title<-"Conference Proceedings from ICALT, ECTEL, CAL, ICHL and ICWL"
+title<-"TEL Blogs Jan 2009 - Mid Sept 2012"
 
-data.type<-"a" #a = conference abstracts, b = blogs
+data.type<-"b" #a = conference abstracts, b = blogs
 
 #whether to split the dataset by year and create separate plots for each
 split.years=TRUE
 
 # allow for multiple words to be the "word in focus", around which the previous/following words are counted. a compound word is OK (e.g. "learning object")
-focus.words=c("analytics")
-focus.placeholder<-"analytics" #used as the label in the visualistion to represent the focus word(s)
+focus.words=c("cloud")
+focus.placeholder<-"cloud" #used as the label in the visualistion to represent the focus word(s)
 
-## ADJUST THESE to get "nice" results (otherwise may be over-sparse, "no useful results" or over-dense)
 #the quantile to be applied to adjacent word frequency when visualising (only cases above the cut appear)
-cut.quantile<-0.5#0.95
-cut.quantile.split<-0.1 #used for a split-off year. should be smaller than cut.quantile otherwise they are sparse
+cut.quantile<-0.99
+cut.quantile.split<-0.98 #used for a split-off year. often smaller than cut.quantile otherwise they are sparse
 
 source(paste(base.dir,"Adjacent Words/AdjacentWords.R", sep="/"))
--- Adjacent Words/Union C 2005-2012 Init.R
+++ Adjacent Words/Union C 2005-2012 Init.R
@@ -2,7 +2,7 @@
 ## Run Properties - dependent on the source
 base.dir<-"/home/arc1/R Projects/Text Mining Weak Signals"
 source.dir<-paste(base.dir,"Source Data",sep="/")
-set.name<-"Union C 2006 to 2011"
+set.name<-"Union C 2006 to 2012"
 output.dir<-paste("/home/arc1/R Projects/Text Mining Weak Signals Output/Adjacent Words",set.name,sep="/")
 #brew.dir<-paste(base.dir,"AdjacentWords",sep="/")
 
@@ -19,7 +19,7 @@ setwd(output.dir)
 #                   "CAL Abstracts 2007-2011 with metrics.csv")
 set.csv<-NA
 sqlite.filename<-"TMWS Data A.sqlite" #set this to NA to use [deprecated] option a
-sql<-"SELECT year, title, abstract FROM abstract WHERE year BETWEEN '2005' AND '2011'"
+sql<-"SELECT year, title, abstract FROM abstract WHERE year BETWEEN '2005' AND '2012'"
 use.sqlite<-!is.na(sqlite.filename)
 
 title<-"Conference Proceedings from ICALT, ECTEL, CAL, ICHL and ICWL"
@@ -30,12 +30,12 @@ data.type<-"a" #a = conference abstracts, b = blogs
 split.years=TRUE
 
 # allow for multiple words to be the "word in focus", around which the previous/following words are counted. a compound word is OK (e.g. "learning object")
-focus.words=c("analytics")
-focus.placeholder<-"analytics" #used as the label in the visualistion to represent the focus word(s)
+focus.words=c("gesture")
+focus.placeholder<-"gesture" #used as the label in the visualistion to represent the focus word(s)
 
 ## ADJUST THESE to get "nice" results (otherwise may be over-sparse, "no useful results" or over-dense)
 #the quantile to be applied to adjacent word frequency when visualising (only cases above the cut appear)
 cut.quantile<-0.5#0.95
-cut.quantile.split<-0.1 #used for a split-off year. should be smaller than cut.quantile otherwise they are sparse
+cut.quantile.split<-0.0 #used for a split-off year. should be smaller than cut.quantile otherwise they are sparse
 
 source(paste(base.dir,"Adjacent Words/AdjacentWords.R", sep="/"))
