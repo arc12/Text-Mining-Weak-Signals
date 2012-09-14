b420d89761a7028360fe59b435c9c7a5ce623d07
Adam Cooper <a.r.cooper@bolton.ac.uk>
2012-09-14 18:31:47 +0100

    inconsequential

--- Abstract Acquisition Scripts/wrap inproceedings.sh
+++ Abstract Acquisition Scripts/wrap inproceedings.sh
@@ -1,24 +1,24 @@
 #makes the inproceedings.xml file be "proper". Also removes unhelpful HTML tags as mixed content is unhelpful!
-echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<root>" > ./inproceedings.tmp
-cat "ECTEL inproceedings 2011.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
-echo "\n</root>" >> ./inproceedings.tmp
-mv "ECTEL inproceedings 2011.xml" "ECTEL inproceedings 2011.old"
-mv inproceedings.tmp "ECTEL inproceedings 2011.xml"
+#echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<root>" > ./inproceedings.tmp
+#cat "ECTEL inproceedings 2012.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
+#echo "\n</root>" >> ./inproceedings.tmp
+#mv "ECTEL inproceedings 2012.xml" "ECTEL inproceedings 2012.old"
+#mv inproceedings.tmp "ECTEL inproceedings 2012.xml"
 
 echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<root>" > ./inproceedings.tmp
-cat "ICALT inproceedings 2011.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
+cat "ICALT inproceedings 2012.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
 echo "\n</root>" >> ./inproceedings.tmp
-mv "ICALT inproceedings 2011.xml" "ICALT inproceedings 2011.old"
-mv inproceedings.tmp "ICALT inproceedings 2011.xml"
+mv "ICALT inproceedings 2012.xml" "ICALT inproceedings 2012.old"
+mv inproceedings.tmp "ICALT inproceedings 2012.xml"
 
 echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<root>" > ./inproceedings.tmp
-cat "ICWL inproceedings 2011.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
+cat "ICWL inproceedings 2012.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
 echo "\n</root>" >> ./inproceedings.tmp
-mv "ICWL inproceedings 2011.xml" "ICWL inproceedings 2011.old"
-mv inproceedings.tmp "ICWL inproceedings 2011.xml"
+mv "ICWL inproceedings 2012.xml" "ICWL inproceedings 2012.old"
+mv inproceedings.tmp "ICWL inproceedings 2012.xml"
 
 echo "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<root>" > ./inproceedings.tmp
-cat "ICHL inproceedings 2011.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
+cat "ICHL inproceedings 2012.xml" | sed -e 's/<i>/ /'  -e 's/<\/i>/ /'  >> ./inproceedings.tmp
 echo "\n</root>" >> ./inproceedings.tmp
-mv "ICHL inproceedings 2011.xml" "ICHL inproceedings 2011.old"
-mv inproceedings.tmp "ICHL inproceedings 2011.xml"
+mv "ICHL inproceedings 2012.xml" "ICHL inproceedings 2012.old"
+mv inproceedings.tmp "ICHL inproceedings 2012.xml"
